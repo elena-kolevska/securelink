b928264c93d42dbe5910d9d36b5637cbebe4f305
Miss Lv <lv@devadmin.com>
2017-06-26 23:58:39 +0100

    Fix typo

--- exampleGenerateLink.php
+++ exampleGenerateLink.php
@@ -7,7 +7,7 @@ $slink = new SecureLink(LINK_TEMPLATE, SIGN_TEMPLATE, SECRET, 3600);
 
 $SERVER['REMOTE_ADDR'] = '127.0.1.8';
 
-if (!$slink->generate('/pm/20170320/1489995043_465442_371700.flv.mp4', $SERVER['REMOTE_ADDR'], MASK)) {
+if (!$slink->generate('/pm/20170320/1489995043_465442_371700.flv.mp4', $_SERVER['REMOTE_ADDR'], MASK)) {
 	echo "Error: ".$slink->err()."\n";
 	exit();
 }
